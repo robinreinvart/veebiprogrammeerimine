8e7a2103d3a577c073fd0ecfe85c921af97538ec
Sten-Robin Reinvart <robinreinvart@noreply.github.com>
2019-09-09 11:21:41 +0300

    õppetöö jaoks failistruktuur loodud

---
+++ try.php
@@ -0,0 +1,36 @@
+<?php
+  $userName = "Robin Reinvart";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  if($hourNow < 8){
+	$partOfDay = "varane hommik";  
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+   <?php
+	echo $userName;
+   ?>
+   progreb veebi</title>
+</head>
+<body>
+  <?php
+	echo "<h1>" .$userName ." koolitöö leht</h1>";
+  ?>
+  <p>See leht on loodud koolis õppetöö raames ja ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p>Lehe avamise hetkel oli aeg:
+  <?php
+  echo $fullTimeNow
+  ?>
+  .</p>
+  <?php
+    echo "<p> Lehe avamise hetkel oli $partOfDay. </p>";
+  ?>
+</body>
+</html>
